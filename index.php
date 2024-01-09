ccd12e55738a12ce38c4bd023c3d60bfd5467257
dieromeo <diegoromeo99@icloud.com>
2024-01-09 15:01:07 +0100

    feat: tabelle hotel

--- index.php
+++ index.php
@@ -46,24 +46,45 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Php hotel</title>
 </head>
 
 <body>
-    <h1>Hotels</h1>
+    <h1 class="text-center py-5">Hotels</h1>
     <?php
 
     foreach ($hotels as $index => $hotel) {
         $index += 1;
-        echo "<p>Hotel $index</p>";
+        // crea una tabella per ogni hotel 
+        echo '<table class="table table-info mb-5">
+        <thead>
+          <tr>
+          <th scope="col" colspan= 2 class= "text-center text-uppercase py-3"> Hotel ' . $index . '</th> 
+          </tr>
+        </thead>
+        <tbody>';
         foreach ($hotel as $hotel_key => $hotel_info) {
-            echo $hotel_key . ': ' . $hotel_info;
-            echo '<br>';
+            // verifica se sta ciclando sul parcheggio 
+            if ($hotel_key === 'parking') {
+                // crea una variabile da stampare al posto del valore del parcheggio 
+                $presenza_parcheggio = $hotel_info ? 'Yes' : 'No';
+                echo '<tr>
+                <th scope="row">' . $hotel_key . '</th>
+                <td>' . $presenza_parcheggio . '</td>
+              </tr>';
+            } else { // stampa la chiave con il suo valore
+                echo '<tr>
+                <th scope="row">' . $hotel_key . '</th>
+                <td>' . $hotel_info . '</td>
+              </tr>';
+            }
         }
-        echo '<hr>';
     }
 
     ?>
+
 </body>
 
 </html>
